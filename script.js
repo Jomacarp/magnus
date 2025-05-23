0d7b70e8e84ac2e5922f6b93870ac234297f5644
Jor <jmurgautn@gmail.com>
2025-05-22 22:57:45 -0300

    actualizacion 23hs

--- script.js
+++ script.js
@@ -15,3 +15,22 @@ function validarStockAlimentos() {
     
     document.getElementById('alertaStock').textContent = mensaje;
     }
+
+ function agregarVacunas(){
+  const corral = document.getElementById('corral1').value;
+  const cantidad = document.getElementById('cantidad').value;
+  const fecha = document.getElementById('fecha').value;
+  if(!corral||!cantidad||!fecha){
+    alert('Faltan datos')
+    return;
+  }
+  const tabla =document.getElementById('Tabla-Vacunacion').getElementsByTagName('tbody')[0];
+  const nuevafila =tabla.insertRow();
+  nuevafila.insertCell(0).textContent= corral;
+  nuevafila.insertCell(1).textContent= cantidad;
+  nuevafila.insertCell(2).textContent= fecha;
+  
+  document.getElementById('corral1').selectedIndex =0;
+  document.getElementById('cantidad').value= '';
+  document.getElementById('fecha').value= '';
+ }
